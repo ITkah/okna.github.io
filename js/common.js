3c2ca4546faedb5aa6d89c33b2df8c15afc40290
ITkah <ilatkacenko132@gmail.com>
2021-04-28 16:29:57 +0300

    asd

--- js/common.js
+++ js/common.js
@@ -9,3 +9,25 @@ callMenu[0].onclick = function() {
 closeMenu[0].onclick = function() {
     nav[0].classList.remove("nav_active");
 }
+
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        nav[0].classList.remove("nav_active");
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.scrollto').offsetHeight;
+
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset + 150;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
