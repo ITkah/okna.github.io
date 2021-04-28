2c529d6fe63d811652365cc65944f7fe8986ddf1
ITkah <ilatkacenko132@gmail.com>
2021-04-28 22:38:58 +0300

    asd

--- js/common.js
+++ js/common.js
@@ -2,6 +2,8 @@ var callMenu = document.getElementsByClassName('menu');
 var nav = document.getElementsByClassName('navigation');
 var closeMenu = document.getElementsByClassName('close');
 
+
+
 callMenu[0].onclick = function() {
     nav[0].classList.toggle("nav_active");
 }
