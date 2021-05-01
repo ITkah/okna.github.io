296ce81e157ef1d50677cc91e0fdefb5c5abe3d7
ITkah <ilatkacenko132@gmail.com>
2021-05-01 16:21:04 +0300

    asd

--- js/common.js
+++ js/common.js
@@ -25,7 +25,7 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
         const topOffset = document.querySelector('.scrollto').offsetHeight;
 
         const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset + 150;
+        const offsetPosition = elementPosition - topOffset - 30;
 
         window.scrollBy({
             top: offsetPosition,
@@ -33,3 +33,32 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
         });
     });
 });
+
+
+(function() {
+    'use strict';
+  
+    function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+  
+      if (scrolled > coords) {
+        goTopBtn.classList.add('back_to_top-show');
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.remove('back_to_top-show');
+      }
+    }
+  
+    function backToTop() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+      }
+    }
+  
+    var goTopBtn = document.querySelector('.back_to_top');
+  
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+  })();
